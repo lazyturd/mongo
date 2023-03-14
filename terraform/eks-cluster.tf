f00eac196f177046ce73eccda551c971015a4ca3
Ayo <ayorexyshittu@gmail.com>
2023-03-14 12:38:10 +0100

    j

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -2,9 +2,27 @@ module "eks" {
     source  = "terraform-aws-modules/eks/aws"
     version = "~> 19.0"
     cluster_name = "myapp-eks-cluster"
-    cluster_version = "1.24"
+    cluster_version = "1.25"
 
     cluster_endpoint_public_access  = true
+
+    cluster_addons = {
+    coredns = {
+      preserve    = true
+      most_recent = true
+
+      timeouts = {
+        create = "25m"
+        delete = "10m"
+      }
+    }
+    kube-proxy = {
+      most_recent = true
+    }
+    vpc-cni = {
+      most_recent = true
+    }
+  }
 
     vpc_id = module.myapp-vpc.vpc_id
     subnet_ids = module.myapp-vpc.private_subnets
