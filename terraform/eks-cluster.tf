2bc81df5296d52f9f233964af03841839a16ed43
Ayo <ayorexyshittu@gmail.com>
2023-03-15 02:09:05 +0100

    version

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -38,7 +38,7 @@ module "eks" {
             max_size = 3
             desired_size = 2
 
-            instance_types = ["t2.micro"]
+            instance_types = ["t3.medium"]
         }
     }
 }
--- terraform/modules/kube-prometheus/main.tf
+++ terraform/modules/kube-prometheus/main.tf
@@ -1,6 +1,6 @@
 resource "helm_release" "kube-prometheus" {
   name       = "kube-prometheus-stackr"
-  version    = "36.2.0"
+  version    = "45.7.1"
   repository = "https://prometheus-community.github.io/helm-charts"
   chart      = "kube-prometheus-stack"
 }
